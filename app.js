13ec6d594533f5a4ac69a764dabece897b8e291a
chefkasperson <chefkasperson@gmail.com>
2020-08-17 22:33:56 -0700

    added basic server code

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(express.static("public"))
+
+app.listen(3000, function() {
+    console.log("Server has started on port 3000")
+})
