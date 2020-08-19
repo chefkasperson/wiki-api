0778bfd4af75feff2755bc0ad635c23a46d60eae
chefkasperson <chefkasperson@gmail.com>
2020-08-18 21:44:20 -0700

    added delete route for all articles

--- app.js
+++ app.js
@@ -45,6 +45,16 @@ app.post("/articles", function(req, res){
     })
 })
 
+app.delete("/articles", function(req, res) {
+    Article.deleteMany(function(err){
+        if (!err) {
+            res.send('articles were successfully deleted')
+        } else {
+            res.send(err)
+        }
+    })
+})
+
 app.listen(3000, function() {
     console.log("Server has started on port 3000")
 })
